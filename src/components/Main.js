28ddb224eecb5ad75053276e824ce3017fbd2253
Justine <ujusine@yahoo.com>
2023-07-31 19:56:39 +0800

    add message for search

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,32 +1,34 @@
 import React from "react";
 
 const Main = (props) => {
-	return (
-		<div className="food-wrap">
-			{props.foods.map((food) => (
-				<div className="card" key={food.recipe.label}>
-					<div className="card-food">
-						<div
-							className="card-top"
-							style={{ backgroundImage: "url(" + food.recipe.image + ")" }}
-						></div>
-						<div className="card-bottom">
-							<h6>{food.recipe.label}</h6>
+  return (
+    <div className="food-wrap">
+      {props.foods.length !== 0
+        ? props.foods.map((food) => (
+            <div className="card" key={food.recipe.label}>
+              <div className="card-food">
+                <div
+                  className="card-top"
+                  style={{ backgroundImage: "url(" + food.recipe.image + ")" }}
+                ></div>
+                <div className="card-bottom">
+                  <h6>{food.recipe.label}</h6>
 
-							<ul className="ingredients">
-								{food.recipe.ingredients.map((ingredient) => (
-									<li>{ingredient.text}</li>
-								))}
-							</ul>
-							<a href={food.recipe.url} target="_blank">
-								SEE MORE
-							</a>
-						</div>
-					</div>
-				</div>
-			))}
-		</div>
-	);
+                  <ul className="ingredients">
+                    {food.recipe.ingredients.map((ingredient) => (
+                      <li>{ingredient.text}</li>
+                    ))}
+                  </ul>
+                  <a href={food.recipe.url} target="_blank">
+                    SEE MORE
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))
+        : "Search for food."}
+    </div>
+  );
 };
 
 export default Main;
